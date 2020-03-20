1addecb6c2de8de91292cbb15ab73635903e4951
oyunbyamba <oyunbyamba@cc.edu.mn>
2020-03-20 15:24:56 +0800

    Хэрэглэгчдийн бичсэн утгуудыг вэбээс авдаг болгох

--- app.js
+++ app.js
@@ -1,21 +1,41 @@
 // Дэлгэцтэй ажиллах контроллер
-var uiController = (function() {})();
+var uiController = (function() {
+  var DOMStrings = {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    addBtn: ".add__btn"
+  };
+  return {
+    getInput: function() {
+      return {
+        type: document.querySelector(DOMStrings.inputType).value,
+        description: document.querySelector(DOMStrings.inputDescription).value,
+        value: document.querySelector(DOMStrings.inputValue).value
+      };
+    },
+    getDOMStrings: function() {
+      return DOMStrings;
+    }
+  };
+})();
 
 // Санхүүгийн тооцоололтой ажиллах контроллер
 var financeController = (function() {})();
 
 // Эвентүүдтэй ажиллах буюу програмын холбогч контроллер
 var appController = (function(uiContr, fnContr) {
+  var DOM = uiController.getDOMStrings();
   var ctrlAddItem = function() {
     // 1. Оруулах өгөгдлийг дэлгэцээс олж авна.
-    console.log("Дэлгэцээс өгөгдөл авах хэсэг");
+    console.log(uiController.getInput());
 
     // 2. Олж авсан өгөгдлүүдээ санхүүгийн контроллерт дамжуулж боловсруулан хадгална.
     // 3. Олж авсан өгөгдлүүдээ вэбийн тохирох  хэсэгт гаргана.
     // 4. Төсвийг тооцоолно.
     // 5. Эцсийн үлдэгдэл, тооцоог дэлгэцэнд гаргана.
   };
-  document.querySelector(".add__btn").addEventListener("click", function() {
+  document.querySelector(DOM.addBtn).addEventListener("click", function() {
     ctrlAddItem();
   });
   document.addEventListener("keypress", function(event) {
